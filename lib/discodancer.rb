c5beecfc3d6e6063510583bff78995ce89dcfded
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-06-06 00:02:30 +0900

    Basic periodic crawl logic

--- lib/discodancer.rb
+++ lib/discodancer.rb
@@ -1,4 +1,23 @@
-class Discodancer
-end
+require 'sequel'
+require 'logger'
+require 'timers'
+
+DB = Sequel.connect('sqlite://discodancer.db')
 
 require_relative 'commands'
+require_relative 'crawler'
+
+class Discodancer
+  def initialize
+    @db = DB
+    @logger = Logger.new($stdout)
+    @crawler = Crawler.new(db: @db, logger: @logger)
+    @timers = Timers::Group.new
+  end
+
+  def run
+    @timers.every(10) { @crawler.crawl }
+    loop { @timers.wait }
+  end
+end
+
