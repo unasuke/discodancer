55960fc938c40f899f0606af855937de78281cb6
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-06-07 23:35:19 +0900

    Crawl logic

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,3 +1,5 @@
+require 'faraday'
+require 'rss'
 require_relative 'models'
 
 class Discodancer
@@ -9,6 +11,30 @@ class Discodancer
 
     def crawl
       @logger.info 'Crawling websites'
+      Website.eager(:webhooks).all.each do |website|
+        next unless website.need_retrieve?
+
+        last_updated_at = Time.parse("2000-01-01 00:00:00")
+        begin
+          feed = website.fetch_feed
+          feed.items.filter do |item|
+            item.updated.content > website.last_checked_at
+          end.sort_by do |entry|
+            entry.updated.content
+          end.each do |entry|
+            last_updated_at = entry.updated.content if entry.updated.content > last_updated_at
+            website.webhooks.each do |webhook|
+              @logger.info "Post entry to webhook id:#{webhook.id} #{webhook.workspace} ##{webhook.channel}"
+              webhook.post!(website, entry)
+            end
+          end
+          website.last_checked_at = Time.now
+          website.last_updated_at = last_updated_at
+          website.save
+        rescue => e
+          @logger.error "Error crawling website id:#{website.id} #{website.url}: #{e.message}"
+        end
+      end
     end
   end
 end
