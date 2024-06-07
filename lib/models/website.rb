86b0e545053ca9654aaca7b711aa0ed5238c9d46
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-06-07 23:35:03 +0900

    Tweak website model

--- lib/models/website.rb
+++ lib/models/website.rb
@@ -1,4 +1,6 @@
 require 'sequel'
+require 'faraday'
+require 'rss'
 
 class Discodancer
   class Website < Sequel::Model
@@ -6,12 +8,22 @@ class Discodancer
 
     def before_create
       self.created_at ||= Time.now
+      self.last_checked_at ||= Time.now
       super
     end
 
     def before_destroy
       DB[:webhooks_websites].where(website_id: self.id).delete
       super
+    end
+
+    def need_retrieve?
+      # TODO: iikanjini yaru
+      true
+    end
+
+    def fetch_feed
+      RSS::Parser.parse(Faraday.get(self.url).body)
     end
   end
 end
