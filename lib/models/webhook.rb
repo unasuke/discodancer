5d2787d60ccaaa9078e26c3cdb3f5d4f7395f3fe
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-06-07 23:34:32 +0900

    Post to discord webhook

--- lib/models/webhook.rb
+++ lib/models/webhook.rb
@@ -1,4 +1,7 @@
 require 'sequel'
+require 'json'
+require 'sanitize'
+require 'uri'
 
 class Discodancer
   class Webhook < Sequel::Model
@@ -12,6 +15,43 @@ class Discodancer
     def before_destroy
       DB[:webhooks_websites].where(webhook_id: self.id).delete
       super
+    end
+
+    def post!(website, entry)
+      case self.provider
+      when "discord"
+        post_to_discord(website, entry)
+      else
+        raise "Unknown provider #{self.provider}"
+      end
+    end
+
+    private def post_to_discord(website, entry)
+      Faraday.post(self.url) do |request|
+        request.headers['Content-Type'] = 'application/json'
+        request.body = discord_webhook_body(website, entry)
+      end
+    end
+
+    private def discord_webhook_body(website, entry)
+      entry_url = URI.parse(entry.link.href).tap do |uri|
+        website_uri = URI.parse(website.url)
+        uri.host = website_uri.host unless uri.host
+        uri.scheme = website_uri.scheme unless uri.scheme
+      end.to_s
+      summary = Sanitize.fragment((entry&.summary&.content || entry&.content&.content || "")).gsub("\n", '').strip
+      summary = "\r" + summary unless summary.empty?
+      summary = summary[0..150] + "..." if summary.length > 150
+      description = Sanitize.fragment(entry.title.content) + summary + "\r<#{entry_url}>"
+      JSON.dump({
+        embeds: [
+          {
+            title: website.name,
+            description: description,
+            timestamp: entry.updated.content.iso8601,
+          }
+        ],
+      })
     end
   end
 end
