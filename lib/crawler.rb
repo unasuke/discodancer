5473ebdefe3094f30f910c95c2ea669f7a88ae5e
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-06-28 20:26:11 +0900

    Fix logic

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -14,25 +14,26 @@ class Discodancer
       Website.eager(:webhooks).all.each do |website|
         next unless website.need_retrieve?
 
-        last_updated_at = Time.parse("2000-01-01 00:00:00")
+        last_updated_at = website.last_updated_at || Time.parse("2000-01-01 00:00:00")
         begin
           feed = website.fetch_feed
           feed.items.filter do |item|
-            item.updated.content > website.last_checked_at
+            item.updated.content.localtime > website.last_updated_at
           end.sort_by do |entry|
-            entry.updated.content
+            entry.updated.content.localtime
           end.each do |entry|
-            last_updated_at = entry.updated.content if entry.updated.content > last_updated_at
+            last_updated_at = entry.updated.content.localtime if entry.updated.content.localtime >= last_updated_at
             website.webhooks.each do |webhook|
               @logger.info "Post entry to webhook id:#{webhook.id} #{webhook.workspace} ##{webhook.channel}"
               webhook.post!(website, entry)
             end
           end
-          website.last_checked_at = Time.now
           website.last_updated_at = last_updated_at
-          website.save
         rescue => e
-          @logger.error "Error crawling website id:#{website.id} #{website.url}: #{e.message}"
+          @logger.error "Error crawling website id:#{website.id} #{website.url}: #{e.class} #{e.message}"
+        ensure
+          website.last_checked_at = Time.now
+          website.save
         end
       end
     end
